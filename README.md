22cf4151c79557c8d3c1ea14f00e208478df9f39
Mohammed Shariq Nawaz <mohammedshariqn@gmail.com>
2022-06-20 15:34:22 +0530

    Changed routes

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # AnnouncementBoard
+This is an Announcement Board which has been build using 
+## Steps to run
+
+1. _Clone_ the repo
+   `git clone https://github.com/mohammedshariqnawaz/AnnouncementBoard`
+2. Setup _virtual environment_
+   `python -m venv venv/`
+3. _Switch_ to virtual environment for development (\<venv\> is your env name, here "venv")
+   - **Windows** (`cmd`)
+     - _Activate_
+       `<venv>\Scripts\acitvate`
+     - _Deactivate_
+       `<venv>\Scripts\deacitvate`
+   - **Linux/Mac**
+     - _Activate_
+       `source <venv>/bin/activate`
+     - _Deactivate_
+       `deactivate`
+4. _Install_ dependencies
+   `pip install -r requirements.txt`
+5. _Run_ the _app_ with
+   `python app.py`
+6. Open [localhost:5000](http://localhost:5000/) to view your app
--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 master_list = [{
     "id": 3,
     "text": "3",
-    "date": "18/05/2022"
+    "date": "21/05/2022"
   },{
     "id": 2,
     "text": "2",
@@ -25,56 +25,47 @@ master_list = [{
 @app.route("/", methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        empty_dict = {}
-        empty_dict["id"]  = len(master_list)+1 #Add id entry
-        empty_dict["text"] = request.form['announcement_text'] #Add text entry
-        empty_dict["date"] = datetime.now().strftime('%d/%m/%Y') #Add date entry 
+        if request.form['submit'] == 'Create':
+            empty_dict = {}
+            empty_dict["id"]  = len(master_list)+1 #Add id entry
+            empty_dict["text"] = request.form['announcement_text'] #Add text entry
+            empty_dict["date"] = datetime.now().strftime('%d/%m/%Y') #Add date entry 
+            
+            master_list.insert(0,empty_dict) #Insert new entry into first index position of the master list
+            return redirect(url_for('index'))
+
+        elif request.form['submit'] == 'all-button':
+            return render_template('index.html',data = master_list,text="all")
         
-        master_list.insert(0,empty_dict) #Insert new entry into first index position of the master list
-        
-        return redirect(url_for('index'))
+        elif request.form['submit'] == 'today-button':
+            todays_list = [x for x in master_list if x['date'] == datetime.now().strftime('%d/%m/%Y')]
+            return render_template('index.html',data= todays_list ,text="todays")
 
-    return render_template("index.html", data=master_list)
+        elif request.form['submit'] == 'seve-day-button':
+            start_date = datetime.now() - timedelta(days=7) #Get start date
+            end_date = datetime.now() #Get ending date
+            #Filter master list according to last seven days
+            seven_list = [x for x in master_list if datetime.strptime(start_date.strftime('%d/%m/%Y'),'%d/%m/%Y') <= datetime.strptime(x['date'],'%d/%m/%Y') <= datetime.strptime(end_date.strftime('%d/%m/%Y'),'%d/%m/%Y') ]
+            return render_template('index.html',data=seven_list,text="last seven days")
 
-@app.route("/all",methods=['POST'])
-def all():
-    return render_template('index.html',data = master_list)
+        elif request.form['submit'] == 'thirty-day-button':
+            start_date = datetime.now() - timedelta(days=30) #Get start date
+            end_date = datetime.now() #Get ending date
+            #Filter master list according to last thirty days
+            thirty_list = [x for x in master_list if datetime.strptime(start_date.strftime('%d/%m/%Y'),'%d/%m/%Y') <= datetime.strptime(x['date'],'%d/%m/%Y') <= datetime.strptime(end_date.strftime('%d/%m/%Y'),'%d/%m/%Y') ]
+            return render_template('index.html',data = thirty_list,text="last thirty days")
     
-@app.route("/today",methods=['POST'])
-def today():
-    #Filter master list according to today's date
-    todays_list = [x for x in master_list if x['date'] == datetime.now().strftime('%d/%m/%Y')]
-    return render_template('index.html',data= todays_list)
+    return render_template("index.html", data=master_list, text="all")
 
-@app.route("/last_seven_days",methods=['POST'])
-def last_seven_days():
-    
-    start_date = datetime.now() - timedelta(days=7)
-    end_date = datetime.now()
-
-    #Filter master list according to last seven days
-    seven_list = [x for x in master_list if datetime.strptime(start_date.strftime('%d/%m/%Y'),'%d/%m/%Y') <= datetime.strptime(x['date'],'%d/%m/%Y') <= datetime.strptime(end_date.strftime('%d/%m/%Y'),'%d/%m/%Y') ]
-    return render_template('index.html',data=seven_list)
-
-@app.route("/last_thirty_days",methods=['POST'])
-def last_thirty_days():
-    start_date = datetime.now() - timedelta(days=30)
-    end_date = datetime.now()
-
-    #Filter master list according to last thirty days
-    thirty_list = [x for x in master_list if datetime.strptime(start_date.strftime('%d/%m/%Y'),'%d/%m/%Y') <= datetime.strptime(x['date'],'%d/%m/%Y') <= datetime.strptime(end_date.strftime('%d/%m/%Y'),'%d/%m/%Y') ]
-    return render_template('index.html',data = thirty_list)
-
-
-@app.route("/delete/<int:id>", methods=['POST'])
+@app.route("/delete/<int:id>", methods=['GET','POST'])
 def delete(id):
-
     #Find post in master_list with the given id
     for index, post in enumerate(master_list):
         if post['id'] == id:
             del master_list[index] #Delete post from master list 
-
     return render_template('index.html',data=master_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.jinja_env.auto_reload = True
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(host="localhost", port=5000, debug=True)
